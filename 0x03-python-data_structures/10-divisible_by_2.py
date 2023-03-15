959a72cc85093562fec1dd520541dafa97cdb390
ApstK <profking11@gmail.com>
2023-03-15 11:21:28 -0700

    Data Structure in Python

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,20 +1,11 @@
-#include <Python.h>
-#include <object.h>
-#include <listobject.h>
+#!/usr/bin/python3
 
-/**
-* print_python_list_info - prints some basic info about pytohn list
-* @p: python object
-**/
+def divisible_by_2(my_list=[]):
+    new_list = []
 
-void print_python_list_info(PyObject *p)
-{
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *obj = (PyListObject *)p;
-
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-}
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return (new_list)
